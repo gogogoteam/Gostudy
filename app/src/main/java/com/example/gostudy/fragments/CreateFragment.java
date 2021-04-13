f1a15fe2353408e92dde50a4ddeb1d537043d58c
RuiminGao <rpg5384@psu.edu>
2021-04-14 00:46:51 +0800

    Revert "Update CreateFragment.java"
    This reverts commit 6eebfea7a2a481c2f57488c7fd21d3d0e8917101.

--- app/src/main/java/com/example/gostudy/fragments/CreateFragment.java
+++ app/src/main/java/com/example/gostudy/fragments/CreateFragment.java
@@ -146,7 +146,8 @@ public class CreateFragment extends Fragment {
                     getContext().startActivity(i);
 
                 }
-                
+                // create Course for each objects
+                // go home screen? preview?
             }
         });
     }
