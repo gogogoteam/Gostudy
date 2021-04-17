5b1fa95973beb792c29a9a8eb5c5e66500cff934
jasminelin18 <71404828+jasminelin18@users.noreply.github.com>
2021-04-16 20:39:44 -0400

    commit

--- app/src/main/java/com/example/gostudy/fragments/HomeFragment.java
+++ app/src/main/java/com/example/gostudy/fragments/HomeFragment.java
@@ -1,21 +1,39 @@
 package com.example.gostudy.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.gostudy.MainActivity;
 import com.example.gostudy.R;
+import com.example.gostudy.ViewPlanActivity;
+import com.example.gostudy.adapters.PlanAdapter;
+import com.example.gostudy.models.Plan;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    private TextView tvHomePlanName;
+    private RecyclerView rvPlan;
+    ParseUser user;
+    List<Plan> plans;
 
     public HomeFragment(){
         // Required empty public constructor
@@ -32,5 +50,16 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        tvHomePlanName = view.findViewById(R.id.tvHomePlanName);
+        rvPlan = view.findViewById(R.id.rvPlan);
+        user = ParseUser.getCurrentUser();
+        plans = new ArrayList<>();
+
+        PlanAdapter planAdapter = new PlanAdapter(getContext(), plans);
+        rvPlan.setAdapter(planAdapter);
+
+
     }
+
+
 }
